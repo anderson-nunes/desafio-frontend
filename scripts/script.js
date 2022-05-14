0a647e71c0c66a114d76427c3a865b576870bdbf
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2022-05-14 13:57:48 -0300

    finalizando fluxo de validação dos campos

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 function diminuir() {
   const event = new Event("input");
+
   const quantidade = document.getElementById("quantidade-stickers");
   const value = parseInt(quantidade.value) || 0;
   if (value > 0) {
@@ -16,23 +17,9 @@ function aumentar() {
   quantidade.value = value + 1;
 
   quantidade.dispatchEvent(event);
-  validateQuantidadeStickers();
 }
 
-function init() {
-  const button = document.getElementById("counter-diminuir");
-  const input = document.querySelector("#quantidade-stickers");
-
-  input.addEventListener("input", function () {
-    if (parseInt(input.value) === 0) {
-      button.setAttribute("disabled", "disabled");
-    } else {
-      button.removeAttribute("disabled");
-    }
-  });
-}
-
-function validarForm() {
+function validateCheckobx() {
   const checkReact = document.getElementById("checkbox-react");
   const checkVue = document.getElementById("checkbox-vue");
   const checkAngular = document.getElementById("checkbox-angular");
@@ -43,12 +30,11 @@ function validarForm() {
 
   if (!checkValidate) {
     fieldsetElement.classList.add("error");
+    return false;
   } else {
     fieldsetElement.classList.remove("error");
+    return true;
   }
-
-  validateQuantidadeStickers();
-  console.log(checkValidate);
 }
 
 function validateQuantidadeStickers() {
@@ -56,9 +42,45 @@ function validateQuantidadeStickers() {
   const validateQuantidadeStickers = parseInt(quantidadeStickers.value) >= 1;
   if (validateQuantidadeStickers === false) {
     quantidadeStickers.classList.add("error");
+    return false;
   } else {
     quantidadeStickers.classList.remove("error");
+    return true;
   }
+}
+
+function validarForm() {
+  const message = document.getElementById("msg");
+  const validateQt = validateQuantidadeStickers();
+  const validateCheck = validateCheckobx();
+
+  if (validateQt && validateCheck) {
+    message.innerHTML = "Dados enviados!!!";
+  } else {
+    message.innerHTML = "";
+  }
+}
+
+function init() {
+  const button = document.getElementById("counter-diminuir");
+  const input = document.querySelector("#quantidade-stickers");
+  const checkboxes = document.querySelectorAll(".checkbox");
+
+  input.addEventListener("input", function () {
+    if (parseInt(input.value) === 0) {
+      button.setAttribute("disabled", "disabled");
+    } else {
+      button.removeAttribute("disabled");
+    }
+
+    validateQuantidadeStickers();
+  });
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+      validateCheckobx();
+    });
+  });
 }
 
 init();
